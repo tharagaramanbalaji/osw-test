d46cd9b8f5d0af71016de7b3f41fb462805e2f32
Tharagaraman <83207969+tharagaramanbalaji@users.noreply.github.com>
2024-10-21 21:24:03 +0530

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,34 @@
+// PROFESSOR
+const swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 30,
+
+  // Pagination bullets
+  //Note:comment to get rid of bullet points
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
